5d14c46504c6c7cb72da43ac66cb472958574756
aliasqardehqani <aliasqar.dehqanii@gmail.com>
2023-01-01 22:45:07 +0330

    add test to account app

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='bookshop22',
+            email="ali2@gmail.com",
+            password="Ali783523"
+        )
+        self.assertEqual(user.username, "bookshop22")
+        self.assertEqual(user.email, "ali2@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='superadmin',
+            email = 'superadmin@gmail.com',
+            password = 'Ali783523'
+        )
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@gmail.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
